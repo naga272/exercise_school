4f8b1789caf74d662a3804cb305eb2be0b021cd9
Nagato272 <114398392+naga272@users.noreply.github.com>
2025-01-21 06:29:39 +0200

    Update local.py

--- bastianello_federico_contravvenzioni/contravvenzioni/shell/local_db/local.py
+++ bastianello_federico_contravvenzioni/contravvenzioni/shell/local_db/local.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm             import sessionmaker, relationship
 
 
-engine = create_engine('sqlite:///shell/local_db/local_contravvenzioni.sqlite', echo=True)
+engine = create_engine('sqlite:///shell/local_db/local_contravvenzioni.sqlite')
 Base = declarative_base()
 
 
